eba650c81bbb3e36a7203922c33579f03b0a3ff3
Sergio Pedri <Sergio0694@live.com>
2015-09-25 18:54:23 +0200

    Added comments and moved the math files into a subdirectory

--- Misc/GC_definitions.h
+++ Misc/GC_definitions.h
@@ -2,13 +2,14 @@
 #define GC_DEFINITIONS
 
 // General libraries
-include <stdio.h>
-include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "Math/GC_math.h"
 
 // Displays an error message and terminates the process
-#define ERROR_HELPER(error) 
-fprintf(stderr, error);    \
-exit(EXIT_FAILURE);        \
+#define ERROR_HELPER(error)    \
+fprintf(stderr, "%s", error);  \
+exit(EXIT_FAILURE);            \
 
 typedef enum { FALSE, TRUE } bool_t;
 
