223c45e14035f41527969e2c8d9748f4f4b996b2
Sergio Pedri <Sergio0694@live.com>
2015-09-25 18:04:35 +0200

    Added some comments in the math file

---
+++ Misc/GC_math.h
@@ -0,0 +1,14 @@
+#ifndef GC_MATH
+#define GC_MATH
+
+/* ---------------------------------------------------------------------
+*  isqrt
+*  ---------------------------------------------------------------------
+*  Description:
+*    Returns the integer approximation by defect of the square root
+*    of the given number, using the Newton's method
+*  Parameters:
+*    value ---> the number to use to calculate the square root */
+int isqrt(int value);
+
+#endif
