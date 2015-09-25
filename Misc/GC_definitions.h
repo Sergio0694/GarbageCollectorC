cdd0ba8fa449d75968ee7025be37b22850fcb66d
Sergio Pedri <Sergio0694@live.com>
2015-09-25 19:02:00 +0200

    Removed duplicated lines

--- Misc/GC_definitions.h
+++ Misc/GC_definitions.h
@@ -4,20 +4,11 @@
 // General libraries
 #include <stdio.h>
 #include <stdlib.h>
-<<<<<<< HEAD
-
-// Displays an error message and terminates the process
-#define ERROR_HELPER(error)      \
-fprintf(stderr, "%s", error);    \
-exit(EXIT_FAILURE);        		 \
-=======
-#include "Math/GC_math.h"
 
 // Displays an error message and terminates the process
 #define ERROR_HELPER(error)    \
 fprintf(stderr, "%s", error);  \
 exit(EXIT_FAILURE);            \
->>>>>>> master
 
 typedef enum { FALSE, TRUE } bool_t;
 
