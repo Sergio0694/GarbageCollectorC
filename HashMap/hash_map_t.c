e7e501dd626509aa36b74216dcd2103c6c574d89
AnSlvt <andrea.slvt94@gmail.com>
2015-09-26 12:56:01 +0200

    fixed hash_map.c

--- HashMap/hash_map_t.c
+++ HashMap/hash_map_t.c
@@ -7,18 +7,10 @@
 /* =========== Local constants ===========*/
 
 #define TRE_QUARTI (3 / 4.0)
-#define SENTINEL (int*)-1
+#define SENTINEL (void*)-1
 #define FIRST_PRIME 257
 
 /* =========== Types used in the file ===========*/
-
-struct hash_map_s
-{
-	pointer_entry_s* map;
-	int current_max_size;
-	int current_size;
-	int prime_for_hash;
-};
 
 struct pointer_entry_s
 {
@@ -26,81 +18,50 @@ struct pointer_entry_s
 	bool_t valid;
 };
 
+typedef pointer_entry_s* pointer_entry_t;
+
+struct hash_map_s
+{
+	pointer_entry_t* map;
+	int current_max_size;
+	int current_size;
+	int prime_for_hash;
+};
+
 /* ===========  PRIVATE FUNCTIONS =========== */
 static hash_map_t custom_hash_map_init(size_t size);
 static int hash_function_1(int* k, int N);
 static int hash_function_2(int* k);
 static void rehash(hash_map_t hm);
-static int find_position(hash_map_t hm, int* k);
-static pointer_entry_s* create_pointer_entry(int* pointer);
+static int find_position(hash_map_t hm, void* k);
+static pointer_entry_s* create_pointer_entry(void* pointer);
 /* ========================================== */
-
-static pointer_entry_s* create_pointer_entry(int* pointer)
-{
-	pointer_entry_s* pointer_entry = (pointer_entry_s*)malloc(sizeof(pointer_entry_s));
-	pointer_entry->pointer = pointer;
-	pointer_entry->valid = TRUE;
-	return pointer_entry;
-}
-
-void mark_pointers_as_invalid(hash_map_t hm)
-{
-	int i;
-	pointer_entry_s* map = hm->map;
-	for (i = 0; i < hm->current_max_sizel; i++)
-	{
-		if (map[i] != NULL && map[i] != SENTINEL)
-		{
-			map[i]->valid = FALSE;
-		}
-	}
-}
-
-mark_as_valid_if_present(hash_map_t hm, void* pointer)
-{
-	// TODO
-}
-
-void deallocate_lost_references(hash_map_t hm)
-{
-	int i;
-	pointer_entry_s* map = hm->map;
-	for (i = 0; i < hm->current_max_sizel; i++)
-	{
-		if (map[i] != NULL && map[i] != SENTINEL)
-		{
-			if (!map[i]->valid)
-			{
-				free(map[i]);
-				map[i] = SENTINEL;
-			}
-		}
-	}
-}
 
 hash_map_t hash_map_init()
 {
 	return custom_hash_map_init(FIRST_PRIME);
 }
 
-bool_t insert_key(hash_map_t hm, int* k)
+bool_t insert_key(hash_map_t hm, void* k)
 {
 	int max_size = hm->current_max_size;
-	int i = hash_function_1(k, hm->prime_for_hash);
-	if (hm->map[i] == NULL || hm->map[i] == SENTINEL)
+	int i = hash_function_1((int*)k, hm->prime_for_hash);
+	if (hm->map[i]->pointer == NULL || hm->map[i]->pointer == SENTINEL)
 	{
+		free(hm->map[i]);
 		hm->map[i] = create_pointer_entry(k);
 		hm->current_size += 1;
 		if ((hm->current_size / (double)max_size) >= TRE_QUARTI) rehash(hm);
 		return TRUE;
 	}
 	int j;
-	int hf2 = hash_function_2(k);
+	int hf2 = hash_function_2((int*)k);
 	for (j = 0; j < max_size; ++j)
 	{
 		int p = (i + j * hf2) % hm->prime_for_hash;
-		if (hm->map[p] == NULL || hm->map[p] == SENTINEL)
+		if (hm->map[p]->pointer == NULL || hm->map[p]->pointer == SENTINEL)
 		{
+			free(hm->map[p]);
 			hm->map[p] = create_pointer_entry(k);
 			hm->current_size += 1;
 			if ((hm->current_size / (double)max_size) >= TRE_QUARTI) rehash(hm);
@@ -110,17 +71,17 @@ bool_t insert_key(hash_map_t hm, int* k)
 	return FALSE;
 }
 
-int* find_key(hash_map_t hm, int* k)
+void* find_key(hash_map_t hm, void* k)
 {
 	int max_size = hm->current_max_size;
-	int i = hash_function_1(k, hm->prime_for_hash);
-	if (hm->map[i] == k || hm->map[i] == NULL) return hm->map[i];
+	int i = hash_function_1((int*)k, hm->prime_for_hash);
+	if (hm->map[i]->pointer == k || hm->map[i]->pointer == NULL)) return hm->map[i]->pointer; 
 	int j;
-	int hf2 = hash_function_2(k);
+	int hf2 = hash_function_2((int*)k);
 	for (j = 0; j < max_size; ++j)
 	{
 		int p = (i + j * hf2) % hm->prime_for_hash;
-		if (hm->map[i] == k || hm->map[i] == NULL) return hm->map[i];
+		if (hm->map[p]->pointer == k || hm->map[p]->pointer == NULL) return hm->map[p]->pointer;
 	}
 	return NULL;
 }
@@ -130,14 +91,49 @@ bool_t replace_key(hash_map_t hm, int* old_key, int* new_key)
 	// TODO
 }
 
-bool_t remove_key(hash_map_t hm, int* k)
+bool_t remove_key(hash_map_t hm, void* k)
 {
 	int i;
 	if ((i = find_position(hm, k)) == -1) return FALSE;
-	free(hm->map[i]);
-	hm->map[i] = SENTINEL;
+	free(hm->map[i]->pointer);
+	hm->map[i]->pointer = SENTINEL;
 	hm->current_size -= 1;
 	return TRUE;
+}
+
+void mark_pointers_as_invalid(hash_map_t hm)
+{
+	int i;
+	pointer_entry_t* map = hm->map;
+	for (i = 0; i < hm->current_max_size; i++)
+	{
+		if (map[i]->pointer != NULL && map[i]->pointer != SENTINEL)
+		{
+			map[i]->valid = FALSE;
+		}
+	}
+}
+
+void mark_as_valid_if_present(hash_map_t hm, void* pointer)
+{
+	int i = find_position(hm, pointer);
+	hm->map[i]->valid = TRUE;
+}
+
+void deallocate_lost_references(hash_map_t hm)
+{
+	int i;
+	pointer_entry_t* map = hm->map;
+	for (i = 0; i < hm->current_max_sizel; i++)
+	{
+		if (map[i]->pointer != NULL && map[i]->pointer != SENTINEL)
+		{
+			if (!map[i]->valid)
+			{
+				remove_key(hm, map[i]->pointer);
+			}
+		}
+	}
 }
 
 void hash_map_free(hash_map_t hm)
@@ -146,8 +142,12 @@ void hash_map_free(hash_map_t hm)
 	int i;
 	for (i = 0; i < max_size; ++i)
 	{
-		if (hm->map[i] != NULL && hm->map[i] != SENTINEL) free(hm->map[i]);
-		else if (hm->map[i] == SENTINEL) hm->map[i] = NULL;
+		if (hm->map[i] != NULL && (hm->map[i]->pointer == SENTINEL || hm->map[i]->pointer == NULL)) free(hm->map[i]);
+		else if (hm->map[i] != NULL && hm->map[i]->pointer != NULL)
+		{
+			free(hm->map[i]->pointer);
+			free(hm->map[i]);
+		}
 	}
 	free(hm->map);
 	free(hm);
@@ -156,14 +156,25 @@ void hash_map_free(hash_map_t hm)
 static hash_map_t custom_hash_map_init(size_t size)
 {
 	hash_map_t to_return = (hash_map_t)malloc(sizeof(struct hash_map_s));
-	to_return->map = (int**)malloc(size * sizeof(int*));
+	to_return->map = (pointer_entry_t*)malloc(size * sizeof(pointer_entry_t));
+
+	int i;
+	for (i = 0; i < size; ++i) to_return->map[i] = create_pointer_entry(NULL);
+
 	to_return->current_max_size = size;
 	to_return->current_size = 0;
 	if (is_prime(size)) to_return->prime_for_hash = size;
 	else to_return->prime_for_hash = biggest_previous_prime(size);
-	int i;
-	for (i = 0; i < size; ++i) to_return->map[i] = NULL;
+
 	return to_return;
+}
+
+static pointer_entry_t create_pointer_entry(void* pointer)
+{
+	pointer_entry_t pointer_entry = (pointer_entry_t)malloc(sizeof(struct pointer_entry_s));
+	pointer_entry->pointer = pointer;
+	pointer_entry->valid = TRUE;
+	return pointer_entry;
 }
 
 static int hash_function_1(int* k, int N)
@@ -178,19 +189,19 @@ static int hash_function_2(int* k)
 	return q - ((int)k % q);
 }
 
-static int find_position(hash_map_t hm, int* k)
+static int find_position(hash_map_t hm, void* k)
 {
 	int max_size = hm->current_max_size;
-	int i = hash_function_1(k, hm->prime_for_hash);
-	if (hm->map[i] == k) return i;
-	else if (hm->map[i] == NULL) return -1;
+	int i = hash_function_1((int*)k, hm->prime_for_hash);
+	if (hm->map[i]->pointer == k) return i;
+	else if (hm->map[i]->pointer == NULL) return -1;
 	int j;
-	int hf2 = hash_function_2(k);
+	int hf2 = hash_function_2((int*)k);
 	for (j = 0; j < max_size; ++j)
 	{
 		int p = (i + j * hf2) % hm->prime_for_hash;
-		if (hm->map[i] == k) return i;
-		else if (hm->map[i] == NULL) return -1;
+		if (hm->map[i]->pointer == k) return i;
+		else if (hm->map[i]->pointer == NULL) return -1;
 	}
 	return -1;
 }
@@ -200,11 +211,11 @@ static void rehash(hash_map_t hm)
 	int size = hm->current_max_size;
 	hash_map_t hm_new = custom_hash_map_init(2 * size);
 	int i;
-	for (i = 0; i < size; ++i) if (hm->map[i] != NULL) insert_key(hm_new, hm->map[i]);
+	for (i = 0; i < size; ++i) if (hm->map[i]->pointer != NULL) insert_key(hm_new, hm->map[i]);
 	free(hm->map);
 	free(hm);
 	hm = custom_hash_map_init(hm_new->current_max_size);
-	for (i = 0; i < hm->current_max_size; ++i) if (hm_new->map[i] != NULL) insert_key(hm, hm_new->map[i]);
+	for (i = 0; i < hm->current_max_size; ++i) if (hm_new->map[i]->pointer != NULL) insert_key(hm, hm_new->map[i]);
 	free(hm_new->map);
 	free(hm_new);
 }
