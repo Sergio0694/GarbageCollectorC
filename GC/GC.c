2872217988da866aaff08b3929c421b27098800e
Sergio Pedri <Sergio0694@live.com>
2015-09-27 19:40:18 +0200

    Organized the code in the GC.c file and added comments to the GC.h and GC.c files

--- GC/GC.c
+++ GC/GC.c
@@ -5,45 +5,62 @@
 /* =========== Global variables ===========*/
 
 bool_t initialized = FALSE;
-//void* heap_end;
 void* stack_bottom;
 hash_map_t allocation_map;
 
+/* ============================================================================
+*  Init and allocation functions
+*  ========================================================================= */
+
+// Initializes the GarbageCollector in single-thread mode
 void GC_init()
 {
+	// Error check
 	if (initialized)
 	{
 		ERROR_HELPER("The GarbageCollector can't be initialized twice");
 	}
 
-	// Calculate initial parameters
+	// Saves the stack pointer, it will be used during the sweep process
 	stack_bottom = get_stack_pointer();
-	//heap_end = get_heap_pointer();
 
-	// Allocate the hashmap
+	// Allocate the hashmap to hold the references to the allocated memory
 	allocation_map = hash_map_init();
 
+	// Makes sure this function can only be called once
 	initialized = TRUE;
 }
 
+// Wraps the malloc function
 void* GC_alloc(size_t size)
 {
+	// Calls the standard malloc function to allocate memory
 	void* pointer = malloc(size);
-	if (!insert_key(allocation_map, pointer))
+
+	// Stores the reference and the amount of allocated memory
+	if (!insert_key(allocation_map, pointer, size))
 	{
 		ERROR_HELPER("Error inserting a new entry into the hashmap");
 	}
 	return pointer;
 }
 
+// Wraps the realloc function
 void* GC_realloc(void* pointer, size_t size)
 {
+	// Calls the standard realloc function
 	void* new_pointer = realloc(pointer, size);
+
+	// Updates the reference in the hash map
 	replace_key(allocation_map, pointer, new_pointer);
 	return new_pointer;
 }
 
-/* =========== C program memory structure ===========
+/* ============================================================================
+*  Single thread collect section
+*  ============================================================================
+
+>> C program memory structure:
 
 ╔══ Higher addresses
 ║
@@ -59,7 +76,8 @@ void* GC_realloc(void* pointer, size_t size)
 
 ========================================== */
 
-void recursive_graph_mark(void* pointer, size_t allocated_space)
+// Explores a graph of allocated memory nodes and marks all of them as valid
+static void recursive_graph_mark(void* pointer, size_t allocated_space)
 {
 	void* upper_bound = pointer + allocated_space - sizeof(void*);
 	while (pointer < upper_bound)
@@ -74,26 +92,31 @@ void recursive_graph_mark(void* pointer, size_t allocated_space)
 	}
 }
 
+// Automatically deallocates all the memory blocks that can no longer be reached
 void GC_collect()
 {
 	// Set all the pointers in the allocation map as invalid
 	mark_pointers_as_invalid(allocation_map);
 	
-	// Stack sweep and mark
-
+	// Get the pointer to the top of the stack
 	void* address = get_stack_pointer();
 
-	// 4 on 32 bit systems, 8 on 64 bit systems
+	// Calculate the upper address, sizeof(void*) is 4 on 32 bit systems and 8 on 64 bit systems
 	void* upper_bound = stack_bottom - sizeof(void*);
 
+	// Sweep the stack
 	while (address < upper_bound)
 	{
-		if ((size_t allocated_size = find_key(allocation_map, address)) != 0)
+		// Check if the current location is a pointer to an dynamically allocated memory block
+		size_t allocated_size = find_key(allocation_map, address)
+		if (allocated_size != 0)
 		{
+			// Use that block as the root and mark all the memory graph as reachable
 			recursive_graph_mark(address, allocated_size);
 		}
 		address++;
 	}
 
+	// Deallocate all the references that are definitively lost
 	deallocate_lost_references(allocation_map);
 }
--- GC/GC.h
+++ GC/GC.h
@@ -8,17 +8,32 @@
 *  ---------------------------------------------------------------------
 *  Description:
 *    Initializes the GarbageCollector so that it can start checking
-*    for memory leaks and fix them
-*  Parameters:
-*    interval ---> the time in seconds that passes between each memory check.
-*        If 0, the GarbageCollector will collect lost references only when
-*        explicitly called, otherwise it will run automatically.*/
-void GC_init(int interval);
+*    for memory leaks and fix them */
+void GC_init();
 
+/* ---------------------------------------------------------------------
+*  GC_alloc
+*  ---------------------------------------------------------------------
+*  Description:
+*    Wraps the malloc function: allocates a block of memory in the heap
+*    and returns a pointer to the first allocated memory location */
 void* GC_alloc(size_t size);
 
+/* ---------------------------------------------------------------------
+*  GC_realloc
+*  ---------------------------------------------------------------------
+*  Description:
+*    Wraps the realloc function: extends an allocated memory area by
+*    copying all the content of the given memory zone to another one and
+*    returns a pointer to the new area */
 void* GC_realloc(void* pointer, size_t size);
 
+/* ---------------------------------------------------------------------
+*  GC_collect
+*  ---------------------------------------------------------------------
+*  Description:
+*    Automatically identifies all the memory blocks in the heap that
+*    can no longer be reached by user code and deallocates them */
 void GC_collect();
 
 #endif
