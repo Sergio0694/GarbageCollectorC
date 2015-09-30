0f3e54b21ac525f0258577bd84e89679c1a17d0f
Sergio Pedri <Sergio0694@live.com>
2015-10-01 01:19:05 +0200

    ASM code fixed on both x86 and x64 systems

--- GC/MemoryHelper/memory_helper.c
+++ GC/MemoryHelper/memory_helper.c
@@ -3,7 +3,12 @@
 // Returns the stack pointer
 inline void* get_stack_pointer()
 {
+	// Checks the current CPU architecture
+#if defined(__x86_64__) || defined(_M_X64)
+	asm("movl %rsp, %rax");
+#else
 	asm("movl %esp, %eax");
+#endif
 }
 
 // Returns the program break pointer
